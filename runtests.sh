0c1489879aae76d042a0274a3811446aa0f0a613
Paul Healy <lmiphay@gmail.com>
2021-11-12 11:58:48 +0000

    make pylint cache dir if it doesnt exist

--- runtests.sh
+++ runtests.sh
@@ -9,6 +9,10 @@ coverage-${PY} run -m unittest discover -s tests -p 'test*.py'
 coverage-${PY} report --show-missing --fail-under=80
 coverage-${PY} html --directory=coverage.report
 
+if [ ! -d $HOME/.cache/pylint ] ; then
+    mkdir -p $HOME/.cache/pylint || exit 1
+fi
+
 pylint --max-line-length=120 bun/*.py
 flake8 --max-line-length=120 bun/*.py
 
